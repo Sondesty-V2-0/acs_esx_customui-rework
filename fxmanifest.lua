280424c0def599342b0fead7e2852cf489c7d0da
Xeerac <39469899+Xeerac@users.noreply.github.com>
2022-11-13 21:50:55 +0100

    CustomUI - Rework By Xeerac

---
+++ fxmanifest.lua
@@ -0,0 +1,88 @@
+fx_version 'adamant'
+game 'gta5'
+description 'test'
+version '1.0'
+
+
+
+ui_page 'html/hud/ui.html'
+
+client_scripts {
+	'client/cl_customui.lua'
+}
+
+server_scripts {
+	'server/sv_customui.lua'
+}
+
+files {
+	-- Main Images
+	'html/hud/ui.html',
+	'html/hud/style.css',
+	'html/hud/grid.css',
+	'html/hud/main.js',
+	-- Money Images
+	'html/hud/img/credit-card.png',
+	'html/hud/img/money-bag.png',
+	'html/hud/img/wallet.png',
+	-- Job Images
+	'html/hud/img/jobs/banker.png',
+	'html/hud/img/jobs/unicorn.png',
+	'html/hud/img/jobs/bus.png',
+	'html/hud/img/jobs/cardealer.png',
+	'html/hud/img/jobs/reporter.png',
+	'html/hud/img/jobs/ambulance.png',
+	'html/hud/img/jobs/tailor.png',
+	'html/hud/img/jobs/fisherman.png',
+	'html/hud/img/jobs/garbage.png',
+	'html/hud/img/jobs/lumberjack.png',
+	'html/hud/img/jobs/slaughterer.png',
+	'html/hud/img/jobs/mecano.png',
+	'html/hud/img/jobs/mechanic.png',
+	'html/hud/img/jobs/miner.png',
+	'html/hud/img/jobs/pizza.png',
+	'html/hud/img/jobs/police.png',
+	'html/hud/img/jobs/realestateagent.png',
+	'html/hud/img/jobs/securoserv.png',
+	'html/hud/img/jobs/sheriff.png',
+	'html/hud/img/jobs/swat.png',
+	'html/hud/img/jobs/fueler.png',
+	'html/hud/img/jobs/trucker.png',
+	'html/hud/img/jobs/taxi.png',
+	'html/hud/img/jobs/deliverer.png',
+	'html/hud/img/jobs/eboueur.png',
+	'html/hud/img/jobs/unemployed.png',
+	'html/hud/img/jobs/ammu.png',
+	'html/hud/img/jobs/bahama.png',
+	'html/hud/img/jobs/ballas.png',
+	'html/hud/img/jobs/bratva.png',
+	'html/hud/img/jobs/casino.png',
+	'html/hud/img/jobs/famillies.png',
+	'html/hud/img/jobs/illegal.png',
+	'html/hud/img/jobs/kano.png',
+	'html/hud/img/jobs/ms13.png',
+	'html/hud/img/jobs/nightclub.png',
+	'html/hud/img/jobs/pompiste.png',
+	'html/hud/img/jobs/offambulance.png',
+	'html/hud/img/jobs/offpolice.png',
+	'html/hud/img/jobs/state.png',
+	'html/hud/img/jobs/tabac.png',
+	'html/hud/img/jobs/unicorn.png',
+	'html/hud/img/jobs/vigne.png',
+	'html/hud/img/jobs/unemployed2.png',
+	-- Vehicle Images
+	--'html/hud/img/vehicle/damage.png',
+	--'html/hud/img/vehicle/gas.png',
+	--'html/hud/img/vehicle/speed.png',
+	--'html/hud/img/vehicle/locked.png',
+	--'html/hud/img/vehicle/unlocked.png',
+	-- Bottom Round Images
+	'html/hud/img/hunger.png',
+	'html/hud/img/water.png',
+	'html/hud/img/drunk.png',
+	'html/hud/img/speaker1.png',
+	'html/hud/img/speaker2.png',
+	'html/hud/img/speaker3.png',
+	'html/hud/img/backpack.png'
+
+}
